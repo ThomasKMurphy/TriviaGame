4fefaa1d556893e8f9fd5c10059d258a7f7f7678
Thomas Murphy <Tom@Thomass-MacBook-Air.local>
2017-07-16 18:45:35 -0400

    progress

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,27 @@
 var number = 10;
 var intervalId;
 var timerIsRunning = false;
+var questionsVisable = false;
+var displayScore = false;
+
 
 $("#start").click(function(){
     $(".questions").toggleClass();
-    // if timer is done running, have questions dissappear
-    if (number === 0) {
-        $(".questions").toggleClass();
-    }
 });
+
+// make buttons clickable to user only once per 60 second interval
+
+// when buttons are clicked and active, changed background color to: #00FFFF
+
 
 $("#start").click(function() {
     console.log("button was clicked");
-// make button clickable to user only once per 60 second interval
 
     function begin() {
         intervalId = setInterval(decrement, 1000);
         timerIsRunning = true;
+        questionsVisable = true;
+       
     }
 
     function decrement() {
@@ -32,11 +37,20 @@ $("#start").click(function() {
     function stop() {
         clearInterval(intervalId);
         timerIsRunning = false;
+        questionsVisable = false;  
+        
     }
 
     if (timerIsRunning === false){
         (number = 10);
+       
     }
+    
+        // when timeIsRunning === false, have questions visibility: hidden
+
+        // assign "correct" true and false values to each question
+
+        // when timeIsRunning === false, display user score in 0/10 form in <div class="score">
 
     begin();
 
