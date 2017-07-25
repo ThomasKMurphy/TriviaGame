3ba4e145628ed32fc8a094f0b8d199a4d9f2c28c
Thomas Murphy <Tom@Thomass-MacBook-Air.local>
2017-07-25 11:37:45 -0400

    fixed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,70 +1,169 @@
-var number = 30;
-var intervalId;
-var timerIsRunning = false;
-var questionsVisable = false;
-var score = 0;
+$('#start').on('click', function () {
+  game.start()
+})
 
-$("button").click(function() {
-    $(this).toggleClass("active");
-});
+$(document).on('click', '#end', function () {
+  game.done()
+})
 
-$("#start").click(function(){
-    console.log("button was clicked");
-    // make 'start" button clickable to user only once per 60 second interval
+var questions = [{
+  question: 'The national animal of Scotland is the unicorn.',
+  answers: ['True', 'False'],
+  correctAnswer: 'True'
+}, {
+  question: 'The oldest city in the United States is Williamsburg, Virginia.',
+  answers: ['True', 'False'],
+  correctAnswer: 'False'
+}, {
+  question: 'The Pittsburgh Steelers have won the most superbowls.',
+  answers: ['True', 'False'],
+  correctAnswer: 'True'
+}, {
+  question: 'Lobsters have teeth in their stomach.',
+  answers: ['True', 'False'],
+  correctAnswer: 'True'
+}, {
+  question: 'The United States has more natural lakes than any other country.',
+  answers: ['True', 'False'],
+  correctAnswer: 'False'
+}, {
+  question: 'Candy is the most shoplifted food in the United States.',
+  answers: ['True', 'False'],
+  correctAnswer: 'True'
+}, {
+  question: 'For every human on earth, there are 1.6 million ants.',
+  answers: ['True', 'False'],
+  correctAnswer: 'True'
+}, {
+  question: 'Earthworms have 4 hearts.',
+  answers: ['True', 'False'],
+  correctAnswer: 'False'
+}, {
+  question: 'There are more plastic flamingos in the United States than real flamingos.',
+  answers: ['True', 'False'],
+  correctAnswer: 'True'
+}, {
+  question: '3 of Snow White\'s 7 dwarves have names that start with the letter "D".',
+  answers: ['True', 'False'],
+  correctAnswer: 'False'
+}]
 
-    function begin() {
-        $(".questions").toggleClass();
-        intervalId = setInterval(decrement, 1000);
-        timerIsRunning = true;
-        questionsVisable = true;
+var timer = 0
+
+var game = {
+  correct: 0,
+  incorrect: 0,
+  counter: 25,
+  countdown: function () {
+    game.counter--
+    $('#counter').html(game.counter)
+    if (game.counter <= 0) {
+      console.log('Time is up!')
+      game.done()
     }
+  },
 
-    function decrement() {
-        number--;
-        $("#timeleft").html("<h2>" + number + "</h2>");
-        if (number === 0) {
-            stop();
-            $("#timeleft").html("Time!");
-        }
+  start: function () {
+    timer = setInterval(game.countdown, 1000)
+    $('#subwrapper').prepend('<h2><span id = "counter">25</span></h2>')
+    $('#start').remove()
+    for (var i = 0; i < questions.length; i++) {
+      $('#subwrapper').append('<h4>' + questions[i].question + '</h4>')
+      for (var j = 0; j < questions[i].answers.length; j++) {
+        $('#subwrapper').append("<input type='radio' name='question-" + i + "' value='" + questions[i].answers[j] + "'>" + questions[i].answers[j])
+      }
     }
+    $('#subwrapper').append('<br><button id = "end">DONE</button>')
+  },
 
-    function stop() {
-        clearInterval(intervalId);
-        timerIsRunning = false;
-        questionsVisable = false;    
-         if (timerIsRunning === false) {
-            (number = 30); 
-        }
-    }
+  done: function () {
+    $.each($('input[name="question-0"]:checked'), function () {
+      if ($(this).val() === questions[0].correctAnswer) {
+        game.correct++
+      } else {
+        game.incorrect++
+      }
+    })
+    $.each($('input[name="question-1"]:checked'), function () {
+      if ($(this).val() === questions[1].correctAnswer) {
+        game.correct++
+      } else {
+        game.incorrect++
+      }
+    })
+    $.each($('input[name="question-2"]:checked'), function () {
+      if ($(this).val() === questions[2].correctAnswer) {
+        game.correct++
+      } else {
+        game.incorrect++
+      }
+    })
+    $.each($('input[name="question-3"]:checked'), function () {
+      if ($(this).val() === questions[3].correctAnswer) {
+        game.correct++
+      } else {
+        game.incorrect++
+      }
+    })
+    $.each($('input[name="question-4"]:checked'), function () {
+      if ($(this).val() === questions[4].correctAnswer) {
+        game.correct++
+      } else {
+        game.incorrect++
+      }
+    })
+    $.each($('input[name="question-5"]:checked'), function () {
+      if ($(this).val() === questions[5].correctAnswer) {
+        game.correct++
+      } else {
+        game.incorrect++
+      }
+    })
+    $.each($('input[name="question-6"]:checked'), function () {
+      if ($(this).val() === questions[6].correctAnswer) {
+        game.correct++
+      } else {
+        game.incorrect++
+      }
+    })
+    $.each($('input[name="question-7"]:checked'), function () {
+      if ($(this).val() === questions[7].correctAnswer) {
+        game.correct++
+      } else {
+        game.incorrect++
+      }
+    })
+    $.each($('input[name="question-8"]:checked'), function () {
+      if ($(this).val() === questions[8].correctAnswer) {
+        game.correct++
+      } else {
+        game.incorrect++
+      }
+    })
+    $.each($('input[name="question-9"]:checked'), function () {
+      if ($(this).val() === questions[9].correctAnswer) {
+        game.correct++
+      } else {
+        game.incorrect++
+      }
+    })
+    $.each($('input[name="question-10"]:checked'), function () {
+      if ($(this).val() === questions[10].correctAnswer) {
+        game.correct++
+      } else {
+        game.incorrect++
+      }
+    })
 
-    // when timer stops, toggle 'active' buttons back to 'inactive'
+    this.result()
+  },
 
-    // when timer stops, toggle question visibility back to hidden
-        
-    // when timer stops, display user score in <div class="score">
-    // assign "correct" true and false values to each question
-       
-    // for question 1, if user selects "True", score ++;
-    // for question 2, if user selects "False", score ++;
-    // for question 3, if user selects "True", score ++;
-    // for question 4, if user selects "True", score ++;
-    // for question 5, if user selects "False", score ++;
-    // for question 6, if user selects "True", score ++;
-    // for question 7, if user selects "True", score ++;
-    // for question 8, if user selects "False", score ++;
-    // for question 9, if user selects "True", score ++;
-    // for question 10, if user selects "False", score ++;
-
-         // function scoreBoard() {
- //        score++;
- //        $(".score").html("<h2>" + score + "/10</h2>");
- //            if (timerIsRunning === true) {
- //            // do not display in html;
- //           else (timerIsRunner === false) {
- //            // display score in html
- //           }
- //        }
-
-    begin();
-
-});
+  result: function () {
+    clearInterval(timer)
+    $('#subwrapper h3').remove()
+    $('#subwrapper').html('<h3>All done!</h3>')
+    $('#subwrapper').append('<h4>Correct Answers: ' + this.correct + ' <h4>')
+    $('#subwrapper').append('<h4>Incorrect Answers: ' + this.incorrect + '<h4>')
+    $('#subwrapper').append('<h4>Unanswered: ' + (questions.length - (this.incorrect + this.correct)) + '</h4>')
+  }
+}
